7a5685154d69080cee3c46ce4402eb29e71cea41
howooking <junsgk@gmail.com>
2022-11-20 02:05:20 +0900

    https

--- src/pages/VideoDetail.tsx
+++ src/pages/VideoDetail.tsx
@@ -15,7 +15,7 @@ export default function VideoDetail() {
           id="player"
           width="100%"
           height="640"
-          src={`http://www.youtube.com/embed/${video.id}?enablejsapi=1&origin=http://example.com`}
+          src={`https://www.youtube.com/embed/${video.id}?enablejsapi=1&origin=http://example.com`}
           frameBorder="0"
         />
         <div className="p-8">
